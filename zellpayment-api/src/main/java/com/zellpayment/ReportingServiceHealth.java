5a1a9706a185c8bcd238ed35dde6ccfaa1ec4e6d
Shashank K <sharathshashank1986@gmail.com>
2026-09-10 18:53:03 +0530

    project setup

---
+++ zellpayment-api/src/main/java/com/zellpayment/ReportingServiceHealth.java
@@ -0,0 +1,23 @@
+package com.zellpayment;
+
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportingServiceHealth implements HealthIndicator {
+    @Override
+    public @Nullable Health health() {
+        boolean isHealthy = checkReportingServiceHealth();
+        if(isHealthy) {
+            return Health.up().withDetail("Reporting Service", "Available").build();
+        }else{
+            return Health.down().withDetail("Reporting Service", "Unavailable").build();
+        }
+    }
+    private boolean checkReportingServiceHealth() {
+        return true;
+    }
+}
--- zellpayment-api/src/main/java/com/zellpayment/ZellpaymentApiApplication.java
+++ zellpayment-api/src/main/java/com/zellpayment/ZellpaymentApiApplication.java
@@ -1,13 +1,16 @@
 package com.zellpayment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ZellpaymentApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZellpaymentApiApplication.class, args);
+		log.info("ZellpaymentApiApplication started");
 	}
 
 }
