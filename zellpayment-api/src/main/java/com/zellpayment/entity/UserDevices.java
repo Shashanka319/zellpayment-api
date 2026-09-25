c9f85faf9d2c9d137144b0289d2307fda36af297
Shashank K <sharathshashank1986@gmail.com>
2026-09-25 19:24:30 +0530

    Relationsahip mapping

--- zellpayment-api/src/main/java/com/zellpayment/entity/UserDevices.java
+++ zellpayment-api/src/main/java/com/zellpayment/entity/UserDevices.java
@@ -1,23 +1,34 @@
 package com.zellpayment.entity;
 
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "user_devices")
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserDevices {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "id")
-    @SequenceGenerator(name = "id",sequenceName = "seq_user_devices",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "devId")
+    @SequenceGenerator(name = "devId", sequenceName = "seq_user_devices", allocationSize = 1)
     @Column(name = "device_id")
-    private Integer id;
-    @Column(name = "user_id")
-    private Integer user_id;
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
     @Column(name = "device_type")
     private String deviceType;
+
     @Column(name = "device_token")
     private String deviceToken;
+
     @Column(name = "last_login")
     private LocalDateTime lastLogin;
 }
