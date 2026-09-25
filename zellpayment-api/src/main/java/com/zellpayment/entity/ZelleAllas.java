209e4d9839802caefc0b74b728c89ebcc44ffbf5
Shashank K <sharathshashank1986@gmail.com>
2026-09-25 19:25:54 +0530

    Relationsahip mapping

--- zellpayment-api/src/main/java/com/zellpayment/entity/ZelleAllas.java
+++ zellpayment-api/src/main/java/com/zellpayment/entity/ZelleAllas.java
@@ -1,26 +1,38 @@
 package com.zellpayment.entity;
 
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "zelle_alias")
+@Getter
+@Setter
+@NoArgsConstructor
 public class ZelleAllas {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "id")
-    @SequenceGenerator(name = "id",sequenceName ="seq_zelle_alias",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "aliasId")
+    @SequenceGenerator(name = "aliasId", sequenceName = "seq_zelle_alias", allocationSize = 1)
     @Column(name = "alias_id")
-    private Integer aliasId;
-    @Column(name = "user_id")
-    private Integer userId;
+    private Long aliasId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private BankAccounts bankAccount;
+
     @Column(name = "alias_type")
     private String type;
+
     @Column(name = "alias_value")
     private String value;
-    @Column(name = "account_id")
-    private Long  accId;
+
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-
 }
