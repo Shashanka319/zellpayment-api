a3e26543ef719ac7f469892ecd616776f65ddc7f
Shashank K <sharathshashank1986@gmail.com>
2026-09-25 19:24:59 +0530

    Relationsahip mapping

--- zellpayment-api/src/main/java/com/zellpayment/entity/TransactionLimits.java
+++ zellpayment-api/src/main/java/com/zellpayment/entity/TransactionLimits.java
@@ -1,23 +1,35 @@
 package com.zellpayment.entity;
 
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "transaction_limits")
+@Getter
+@Setter
+@NoArgsConstructor
 public class TransactionLimits {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "id")
-    @SequenceGenerator(name = "id",sequenceName =  "seq_transaction_limits",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "limitId")
+    @SequenceGenerator(name = "limitId", sequenceName = "seq_transaction_limits", allocationSize = 1)
     @Column(name = "limit_id")
     private Long limitId;
-    @Column(name = "user_id")
-    private Integer userId;
-    @Column(name = "daily_limit")
-    private Integer dailyLimit;
-    @Column(name = "per_txn_limit")
-    private Integer perTxnLimit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @Column(name = "daily_limit", precision = 12, scale = 2)
+    private BigDecimal dailyLimit;
+
+    @Column(name = "per_txn_limit", precision = 12, scale = 2)
+    private BigDecimal perTxnLimit;
+
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
