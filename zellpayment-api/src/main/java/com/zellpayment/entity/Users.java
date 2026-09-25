d265a00d0749673a727e506c3618037a4bad46d4
Shashank K <sharathshashank1986@gmail.com>
2026-09-25 19:21:01 +0530

    Relationsahip mapping

--- zellpayment-api/src/main/java/com/zellpayment/entity/Users.java
+++ zellpayment-api/src/main/java/com/zellpayment/entity/Users.java
@@ -1,26 +1,56 @@
 package com.zellpayment.entity;
 
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Table(name="users")
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Users {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "user_seq")
-    @SequenceGenerator(name = "user_seq",sequenceName = "seq_users",allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "seq_users", allocationSize = 1)
     @Column(name = "user_id")
-    private Integer userId;
+    private Long userId;
+
     @Column(name = "full_name")
     private String userName;
+
     @Column(name = "email")
     private String userEmail;
+
     @Column(name = "phone_number")
     private String userPhoneNo;
+
     @Column(name = "password_hash")
-    private  String password;
+    private String password;
+
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
     @Column(name = "status")
     private String status;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OTPVerification> otpVerifications = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserDevices> userDevices = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BankAccounts> bankAccounts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TransactionLimits> transactionLimits = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ZelleAllas> zelleAliases = new ArrayList<>();
 }
