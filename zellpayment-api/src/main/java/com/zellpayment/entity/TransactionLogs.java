b9470d27a5acadbae1ec6d106c93f37c4799c27d
Shashank K <sharathshashank1986@gmail.com>
2026-09-25 19:27:39 +0530

    Relationsahip mapping

--- zellpayment-api/src/main/java/com/zellpayment/entity/TransactionLogs.java
+++ zellpayment-api/src/main/java/com/zellpayment/entity/TransactionLogs.java
@@ -1,22 +1,31 @@
 package com.zellpayment.entity;
 
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "transaction_logs")
+@Getter
+@Setter
+@NoArgsConstructor
 public class TransactionLogs {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "logId")
-    @SequenceGenerator(name = "logId",sequenceName = "seq_transaction_logs",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "logId")
+    @SequenceGenerator(name = "logId", sequenceName = "seq_transaction_logs", allocationSize = 1)
     @Column(name = "log_id")
     private Long logId;
-    @Column(name = "transaction_id")
-    private Long transactionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transaction_id", nullable = false)
+    private Transactions transaction;
+
     @Column(name = "message")
     private String message;
+
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-
 }
